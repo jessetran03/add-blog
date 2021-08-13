cd189fdcf974b2931044954a295c947950d7b9d2
Jesse Tran <jessetran03@gmail.com>
2021-08-12 22:27:05 -0500

    Removed content

--- src/blogs.js
+++ src/blogs.js
@@ -1,92 +1,24 @@
 const blogs = [
-    {title: 'Tips for Moving Out',
-    body: <div>One minute you are moving into your apartment, the next your lease is almost up! When it is finally time to pack your things and go, move-out day can seem to be one long and stressful day; but it doesn’t have to be. You can never begin planning too soon and organizing your time to complete tasks prior to move-out, will help make it a seamless process. Start planning your move-out today with these tips to ease the process:
+    {title: 'title',
+    body: 'Body',
+    meta: 'meta',
+    date: 'March 10, 2021'},
 
-    Plan ahead: The more you plan, the less rushed you feel on move-out day, and the less daunting the process becomes. Plan well before your final move-out day. Sometimes it is tricky deciding what must be done sooner rather than later, but the earlier you establish a move-out timeline, the more seamless the transition becomes.
-    
-    Review lease terms: Reviewing the terms of your lease is one of the first steps to complete your move-out. Make sure to look over leasing terms a few months before move-out day to understand your apartment’s required notice timeframe, security deposit policy, and other important information. Are their nail holes that will need to be patched? Did you paint and need to return the wall color to the original one? Speak with the office to ensure you understand the tasks you are responsible for. Schedule a walk-through of your unit ahead of move-out day if you want to be present. Make sure you turn in all of your keys to avoid extra charges. Work out a plan with your roommates if you are not the last one moving out, to ensure common area items are cleaned. Your security deposit will no.  Once you know the day you are moving out, start focusing on the most evident aspect of the process: your belongings. 
-    
-    Start decluttering: First ask yourself what you want to keep and what you can live without. Decluttering before you move reduces your moving costs and allows you to start fresh in your next home. Check out these decluttering tips before you start packing:
-        •	DONATE clothing, furniture, and household items (pots, pans, dishes, bedding, etc.) you no longer need to your favorite charities in town or have a “stuff swap” with friends to trade belongings. 
-        •	SELL items that have a higher value. Familiarize yourself with Facebook Market Place and Craigslist. Post items for sale on your stories on Instagram or have a good old fashioned yard sale.
-        •	RECYCLE or TOSS anything that cannot be donated or sold. Remember, there might be a limit as to what you can put in the trash each week. Pace your trash output so the receptacles aren’t overflowing. 
-    While you give some possessions the boot, start using up the ones that cannot be moved, such as cleaning supplies and frozen food.  As clutter clears and only the essentials remain, step back and decide if movers need to be hired or if your parents will be helping you.
-    
-    Arrange moving help: You will need some help with the move! Research all your options ahead of time. If you need a professional moving service, make the necessary arrangements with the mover at least eight weeks before move-out day. Remember, everyone else is moving out around the same time as you, so plan ahead.  If you can pull off a complete DIY move, obtain moving boxes and pack your car or your friends car. Do not forget to check out free resources for boxes too-Craigslist, Grocery Stores, etc.
-    
-    Who to contact before move: Feel free to take the sole focus off your belonging five to six weeks before move-out day and consider what needs to be done to re-establish yourself in your new home. Start thinking about everyone who needs to be notified of your move. The following is a list of everyone that should be notified before you move:
-        •	Start off with friends, family, and work colleagues if you are making a big move! 
-        •	Ask for referrals when necessary and obtain copies of your personal records. Moving out of state? You might need to updated your motor vehicle registration, licensing documents, and other important paperwork. 
-        •	Contact utility companies like gas, electric, water, cable, and internet (if you set them up individually.) Know your specific move-out and move-in days and schedule your services to disconnect when you leave and reconnect the first day in your new home. 
-        •	If you have any pets or plants, arrange special transportation. 
-        •	Pick one afternoon to visit the post office-USPS to submit a change of address form. You can also update your address online www.usps.com
-    
-    The 2-week countdown: Move out day is only two weeks away, and you are more than prepared for the final day in your apartment. Now to start thinking of all the little loose ends that need tying up. Settle any outstanding bills you have with friends or local merchants. Get your car checked and serviced if you have a long drive, return library books, sell textbooks or return lab equipment to school.  With less than a week to go, turn your attention to inside the apartment. Disconnect the appliances that will come with you and organize them. Pick out personal items that you will need right away in your new home and pack a box that will be by your side for the move. 
-    
-    Day of move - have fun with it: Yes, moving can be stressful, but have fun with it. Treat yourself at the end of moving day with ice cream or a night out with friends. Moving to a new place can be exciting and fun, so have fun surfing the web to look for the new salons, gyms, restaurants, and other places you will be using. Remember to snap a few photos of the big move- a few years from now it will be great to look back and see how far you have come. Good Luck!!`
-    </div>,
-    meta: '%%sitename%% provides tips for moving out at the end of your lease. Learn more!',
-    date: 'March 24, 2021'},
-
-    {title: 'Summer Bucket List after Graduation',
-    body: `It is time to celebrate! We are so excited to congratulate all 2021 graduates for their years of dedication and hard work while pursuing their degrees. Receiving a college degree is a remarkable milestone, and it is important to reward yourself before the next phase of life begins! Maybe one of the biggest things you will miss in college is the time off in the summer, so what better way to take advantage of this summer than with a bucket list! There is plenty to do to make the most out of summer this year and we put together a list to help get you started with the planning! 
-
-    Plan a family trip: Before you start this new chapter, make time for family. Plan a trip to the mountains and go hiking, have a picnic in the park, or find a fun recreational activity the whole family can enjoy. Spend time together near home then take a road trip to a fun destination. Road trips are always a great way to spend quality time with your family. Make your own road trip playlist to remember the year on Spotify. Spend extra family time with your extended family too-grandparents, aunts, uncles, cousins, and younger siblings.  It has been a long year with all the restrictions and those connections need to be renewed. As you move on to your new job and city, keep in touch with family frequently. You might not see them as much, but their support is important. A quick call after a bad day at work can be a big boost!
-    
-    Reconnect with friends: After enjoying some of the summer with your family, visit some old friends from high school you have not seen often during college. Nothing is better than simply catching up with old friends, so don’t worry about planning a lavish trip together. Invite them to your old hang out spots or start new traditions.  
-    
-    Make a summer reading list: After graduation, you will find yourself at home with a lot of time on your hands. What better way to relax at home one last summer than picking out a few books to dive into? Read a book from your favorite author or find an interesting biography about someone who has influenced your life. Set up a hammock outside or take a trip to the beach for a day of reading in the sun. If you know where you are moving next, research some notable authors that were born there and read any of their books that interest you. You can also find plenty of travel style books to read and learn what is fun to do near you, to add to your summer bucket list. 
-    
-    Find a new hobby: Extra time in the summer makes finding a new hobby an exciting option. Ask yourself what you have wanted to do but never had the time or what new expertise you want to take with you into adulthood. Start with photography so you can document all your fun summer adventures! 
-    
-    Become a master chef of your favorite dish: One great bucket list idea that you can use down the road is finding a new love for food. Whether it was making simple meals in-between classes or hanging out with friends at your favorite restaurant, it was easy to adhere to a food routine in college. The summer after college is the perfect time to expand your culinary expertise. Check out new restaurants you have been wanting to try. Start cooking with an easy meal-prep kit like Blue Apron. Then you can start making your own dishes from online recipes or ask a relative to show you a traditional family recipe. Once you become more confident in the kitchen, venture out and explore the food your neighborhood has to offer. Shop for ingredients at your local farmers market or check out some grocery stores that offer fresh organic food and foreign delicacies. Once you get a better idea of the new meals you like, plan a food-centric vacation. Travel to a well-known foodie city that specializes in what you like and take a one-week cooking class. When you return home, you’ll be a master of your new dish, so plan a celebratory dinner with friends and family and showcase your new skill.`,
-    meta: '%%sitename%% gives bucket list ideas for recent graduates. Learn more today!',
+    {title: 'title',
+    body: 'body',
     date: 'April 16, 2021'},
 
-    {title: 'Moving Away from Home for the First Time?',
-    body: `Now is a great time to start making financial decisions that can set you up for long term success. College is expensive and learning to budget and allocate money in different categories will help you track your financial situation throughout the month. 
+    {title: 'title',
+    body: 'body',
+    date: 'May 17, 2021'},
 
-    Create a budget: there are so many budget templates that are available online. Outlining your income and expenses ahead of time sets up a clear financial picture for you for the month. Have money unaccounted for after you have everything budgeted? Splurge on that new restaurant or buy those new shoes!
-    
-    Pay rent on time and in full: Paying rent on time helps build credit and also will help you establish a good rental record for later when you want to rent another apartment or home. This is one of the best ways to start establishing credit and showing you are a good renter. 
-    
-    Get a rewards credit card: getting a credit card is essential for helping you start to establish your credit scores. Use a credit card for one or two small bills each month and pay it off each month or every other month, to show consistent payments. You should also try to only carry a balance of 30% of your credit limit so it doesn’t negatively affect your credit score. As an example, if you credit limit is $500, you should only put $150 on your credit card at a time to stay at the 30% threshold.
-    
-    Use an app to help save: there are so many great apps today that will help save for you without you having to even think about it. Digit withdraws small amounts from the account you link it to based on your spending habits. You can set limits so it does not withdraw if your bank account goes below a certain balance. You can withdraw this money any time to use as well! Want to take a spring break trip or saving for the Holidays? This is a great way to start saving.
-    
-    Pack your lunch: eat at home when you can throughout the week so that your food budget is ready for your action packed weekend! Eating one meal at home each day can save you over $50 a month! 
-    
-    Take baby steps: Nobody said talking about money is fun, and you don’t have to go from 0 to 100 overnight. Even starting with one or two of these habits will help you build a strong financial future that goes well beyond college!`,
-    meta: '%%sitename%% provides tips for moving away from home for the first time. Learn more!',
-    date: 'May 27, 2021'},
+    {title: 'title',
+    body: 'body',
+    date: 'June 12, 2021'},
 
-    {title: 'Get the Most Out of Your College Experience',
-    body: `It’s an exciting time as you get settled into your new home and prepare to start the Fall semester. Staying focused on your classes is important but finding the balance and having fun during your free time is key to getting the most out of your college experience. Here are a few ways to make new friends and get to know your new neighborhood!
-
-    Join a club: Visit your university’s website for a list of clubs and organizations you can join to meet others that share your passion. 
-    
-    University events: These events can be academic and great for networking or social and perfect for meeting new people. An events calendar can be found on your university’s website.
-    
-    Check out local events: Look for an event calendar on the community website or at your local library. Most communities will even have a Facebook page you can follow for notices. The Facebook Events tab will also show upcoming events local to you.
-    
-    Resident events right at home: We also host monthly events with free food and giveaways just for our residents. Follow us on social media and stay tuned!`,
-    meta: '%%sitename%% has a guide for getting the most out of you college experience. Learn more!',
-    date: 'June 22, 2021'},
-
-    {title: 'Easing Back Into Your Social Life',
-    body: `Let’s face it, it’s been a weird year...and a half. We are all excited about getting back into social habits and visiting with friends. But going from couch potato to life of the party can make you feel anxious. 
-
-    Here are our tips for easing back into the best social lifestyle for you:
-    
-    Take baby steps: Nobody is expecting you to go from sitting at home for a year to planning a social event for every night of the week. Plan a few social outings that you are excited about to get you back out into the world!
-    
-    Concentrate on your inner circle: we all have our good friends groups and family (or friends who have become our family!) Make time for the people who matter in your life as a first priority. After your close social group events are established, then pick and choose what other new events or friends you want to open your schedule up for!
-    
-    It’s ok to say no: you DON’T have to do everything, even if people say you do. If an event doesn’t excite you, you don’t have to go. It might be hard to say no at first but making time for yourself to do what you really want to do will make those nights out even better.
-    
-    Try something new: You might be overwhelmed at the thought of trying something new. But when you and your friends all do something that nobody in the group has done, it is easier to get out of your comfort zone with people who are still familiar to you. Go floating down a river, go try an escape room, stay at a local Airbnb together instead of your own dorms or apartments for a change of scenery. These new experiences may just end up being your favorite new group activity!`,
-    meta: '%%sitename%% gives tips for easing back into your social life. Learn more!',
-    date: 'July 16, 2021'},
+    {title: 'title',
+    body: 'body',
+    date: 'July 26, 2021'},
 ]
 
 export default blogs;
